f18c507a5686cc5e43f4f24f6daec6b2874d0493
Sevrinn <sevrinnsevrinn@gmail.com>
2021-08-15 14:24:56 -0700

    fixed some typos

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import All from "./components/All";
 import Create from "./components/Create";
 import Details from "./components/Details";
 import Update from "./components/Update";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Product Manager</h1>
+      <Header />
+
       <Router>
         <All default path="/products" />
         <Create path="/products/new" />
--- server/server.js
+++ server/server.js
@@ -2,9 +2,19 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+//add in cors
+const cors = require("cors");
+
 //configure express app server
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//allow localhost:3000 to talk directly to the backend
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 //configure mongoose to connect
 require("./config/mongoose.config");

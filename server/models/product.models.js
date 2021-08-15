b3b16fce05d00a2879638516fd5be9e03b60dc8c
Sevrinn <sevrinnsevrinn@gmail.com>
2021-08-15 07:43:15 -0700

    basic product model complete

---
+++ server/models/product.models.js
@@ -0,0 +1,23 @@
+//require mongoose
+const mongoose = require("mongoose");
+//create schema with timestamps
+const ProductSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: [true, "Product name is required"],
+      minLength: [3, "Product title must be at least 3 characters."],
+    },
+    price: {
+      type: String,
+      require: [true, "Product price is required"],
+    },
+    description: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+//export model. Collection name and schema are required
+module.exports = mongoose.model("Product", RestaurantSchema);
